cc67a954502e86d41d608d0d208742932bb6466b
Cameron Anderson <cameron.anderson.j@gmail.com>
2023-01-09 16:32:23 -0800

    Generated project files with Spring Initializr. Initial commit

---
+++ src/test/java/guru/springframeword/sfgrecipes/SfgRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframeword.sfgrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
