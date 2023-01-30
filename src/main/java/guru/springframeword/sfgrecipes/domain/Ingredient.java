0d9b01f6cd372b53f627b81c4c8e6d023fdce8a4
Cameron Anderson <cameron.anderson.j@gmail.com>
2023-01-29 19:29:56 -0800

    Create UnitOfMeasure JPA entity and created relationship to Ingredient

--- src/main/java/guru/springframeword/sfgrecipes/domain/Ingredient.java
+++ src/main/java/guru/springframeword/sfgrecipes/domain/Ingredient.java
@@ -11,10 +11,10 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-
-    //private UnitOfMeasure uom;
     @ManyToOne
     private Recipe recipe;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     public Long getId() {
         return id;
@@ -46,5 +46,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
