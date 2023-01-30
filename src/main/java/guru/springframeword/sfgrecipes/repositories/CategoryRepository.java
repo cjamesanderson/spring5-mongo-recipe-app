ef7e368863de70bb75845669e013a4fe5911b3a3
Cameron Anderson <cameron.anderson.j@gmail.com>
2023-01-30 10:40:13 -0800

    Implement Spring Data JPA repositories for UnitOfMeasure and Category and initialize with Spring Data Initializer

--- src/main/java/guru/springframeword/sfgrecipes/repositories/CategoryRepository.java
+++ src/main/java/guru/springframeword/sfgrecipes/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package guru.springframeword.sfgrecipes.repositories;
 import guru.springframeword.sfgrecipes.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/guru/springframeword/sfgrecipes/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframeword/sfgrecipes/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package guru.springframeword.sfgrecipes.repositories;
 import guru.springframeword.sfgrecipes.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
