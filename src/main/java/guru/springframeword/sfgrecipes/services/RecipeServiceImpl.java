ffbf97b608ed599aac5b94ca88a19e42c536f588
Cameron Anderson <cameron.anderson.j@gmail.com>
2023-02-02 10:32:55 -0800

    Create RecipeController and start implementing recipe display page

--- src/main/java/guru/springframeword/sfgrecipes/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframeword/sfgrecipes/services/RecipeServiceImpl.java
@@ -5,6 +5,7 @@ import guru.springframeword.sfgrecipes.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -20,5 +21,17 @@ public class RecipeServiceImpl implements RecipeService{
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
+    }
+
+    @Override
+    public Recipe findById(Long id) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+
+        if (recipeOptional.isEmpty()) {
+            throw new RuntimeException("Recipe Not Found!");
+        }
+
+        return recipeOptional.get();
     }
 }
