e9b40a71b13bc4116939f9b5d06b6f154b726cdf
Cameron Anderson <cameron.anderson.j@gmail.com>
2023-02-16 12:20:54 -0800

    fix UOM integration test

--- src/test/java/guru/springframework/sfgrecipes/repositories/UnitOfMeasureRepositoryTestIT.java
+++ src/test/java/guru/springframework/sfgrecipes/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -1,26 +1,40 @@
 package guru.springframework.sfgrecipes.repositories;
 
+import guru.springframework.sfgrecipes.bootstrap.RecipeBootstrap;
 import guru.springframework.sfgrecipes.domain.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Disabled
 @ExtendWith(SpringExtension.class)
+@DataMongoTest
 class UnitOfMeasureRepositoryTestIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
+        recipeRepository.deleteAll();;
+        unitOfMeasureRepository.deleteAll();
+        categoryRepository.deleteAll();
+
+        RecipeBootstrap recipeBootstrap = new RecipeBootstrap(categoryRepository, unitOfMeasureRepository, recipeRepository);
+
+        recipeBootstrap.run();
     }
 
 
